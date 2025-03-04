f4cc77d1438c8f19cf00d7622a7033b5ee8dee02
joseph <joseph.hoang53@gmail.com>
2025-03-03 17:11:54 -0800

    deleted .gitignore files sorry

--- FollowBotAROS2/src/build/arduino_serial/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mlf_hntr/Projects/ROS2_FollowBot/FollowBotAROS2/src/arduino_serial
-# Build directory: /home/mlf_hntr/Projects/ROS2_FollowBot/FollowBotAROS2/src/build/arduino_serial
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
